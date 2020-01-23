b92282598546dc1f0012d9325a019f6d75dc007b
KerimMbegovic <57753259+KerimMbegovic@users.noreply.github.com>
2020-01-23 13:14:04 +0100

    Update Zadatak1.cpp

--- Zadatak1.cpp
+++ Zadatak1.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <time.h>
 using namespace std;
-
+    /*
+        Unijeti broj elemenata nizai generisati elemente niza u intervalu 5-15.
+        Ispisati "Novonastali niz" tako da svaki element u nizu bude suma prethodnih elemenata.
+        npr. 5 6 7
+        Novonastali niz: 5 11 18
+    
+    */
 int main() {
 
 srand(time(NULL));
