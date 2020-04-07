073abcc1671b7da654deaaad70572fef3a6142ff
Kerim Muharembegović <57753259+KerimMbegovic@users.noreply.github.com>
2020-04-07 15:50:51 +0200

    Add files via upload

--- Fakultet/Zadatak10.cpp
+++ Fakultet/Zadatak10.cpp
@@ -1,51 +1,77 @@
 #include <iostream>
-#include <time.h>
+#include <string.h>
+#include <vector>
 using namespace std;
-
 /*
-    Napraviti program koji generise 20 slucajnih brojeva iz intervala
-    10-50 u niz. Zatim napraviti funkciju koja ce ispisati samo slozene
-    brojeve (brojevi koji nisu prosti).
-    Ispisati pocetni niz i trazene brojeve.
+    Napisati program koji traži od korisnika da unese spisak riječi
+    (broj riječi se prethodno unosi sa tastature), a zatim ispisuje na
+    ekran prvu i poslednju riječ iz spiska po abecednom poretku,
+    kao i popis svih unesenih riječi, ali bez ispisivanja duplikata (tj.
+    bez ispisivanja riječi koje su se već jednom ispisale). Program
+    realizirati korištenjem vektora stringova, odnosno vektora čiji
+    su elementi tipa “string”.
 */
 
-bool prost (int broj){
-    int br=0;
-    for(int i=2;i<broj/2;i++){
-        if(broj%i==0){
-            br++;
-        }
-    }
-    if(br>0){
-        return false;
-    }else
-        return true;
-}
 
-void slozeni (int niz[]){
-cout<<"\nTrazeni brojevi:\n";
-for(int i=0;i<20;i++){
-    if(!prost(niz[i])){
-    cout<<niz[i]<<" ";
-    }
-}
-
-}
 int main(){
 
-srand(time(NULL));
+//Deklaracija vektora i n - broja riječi u njemu
+vector<string> v;
+int n;
 
-int niz[20];
+//Unos broja riječi
+cout<<"Unesite broj rijeci: ";
+cin>>n;
 
-for(int i=0;i<20;i++){
-    niz[i]=rand()%41+10;
+//Unos vektora
+for(int i=0;i<n;i++){
+    cout<<i+1<<". ";
+    //pomoćna varijabla za unos riječi
+    string temp;
+    //unos riječi
+    cin>>temp;
+    //riječ šaljemo u vektor, pomoćna varijabla temp sada možemo ponovo pozvati u sljedećem prolasku kroz for petlju
+    v.push_back(temp);
 }
 
-cout<<"Pocetni niz:\n";
-for(int i=0;i<20;i++){
-    cout<<niz[i]<<" ";
+
+//ISPIS PRVE I POSLJEDNJE RIJEČI PO ABECEDNOM PORETKU
+
+/*
+    Deklaracija dvije string varijable, jedna će pohraniti prvu riječ po abecedi, a druga posljednju.
+    I jedna i druga uzimaju prvu rijec iz vektora, kao referentnu vrijednost.
+*/
+string first=v[0];
+string last=v[0];
+
+//for petlja koja ide od početka do kraja vektora
+for(int i=0;i<v.size();i++){
+    //kroz v[i] prolaze sve riječi iz vektora, od prve do posljednje
+    //ako je ta riječ do koje je petlja došla(v[i]) manja od prve, prva postaje ta riječ.
+    if(v[i]<first) first=v[i];
+    //ako je veca od posljednje, posljednja postaje ta riječ.
+    if(v[i]>last)  last=v[i];
 }
-slozeni(niz);
+
+//Na ovaj nacin smo sigurno dobili prvu i posljednju riječ po abecedi iz unijetog vektora
+//Ipis prve i posljednje riječi
+cout<<"Prva: "<<first<<endl;
+cout<<"Posljednja: "<<last<<endl;
+
+//ISPIS BEZ DUPLIKATA
+cout<<"Bez duplikata: "<<endl;
+//For petlja koja ide od početka do kraja vektora
+for(int i=0;i<v.size();i++){
+    /*
+        Ako je riječ do koje je došla for petlja različita od prve i različita od druge, ispisat će je.
+        Ako uslov nije ispunjen(a to znači da je neka riječ iz vektora ista kao dvije prethodno ispisane)
+        petlja će preći na sljedeću riječ.
+        Na ovaj način će se ispisati samo riječi koje nisu vec prije ispisane. (Ispis bez duplikata)
+    */
+    if(v[i]!=first && v[i]!=last){
+        cout<<v[i]<< " ";
+    }
+}
 
 return 0;
 }
